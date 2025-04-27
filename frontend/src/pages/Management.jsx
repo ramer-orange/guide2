4dccde90a80cdca4c1b7532d0499f34e270ff343
天野海 <kai@amanokainoMacBook-Air-2.local>
2025-04-27 21:29:39 +0900

    #20 管理画面に作成者の名前表示

--- frontend/src/pages/Management.jsx
+++ frontend/src/pages/Management.jsx
@@ -1,12 +1,16 @@
+import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom"
 
 // 管理画面ページ
 
 export default function Management() {
+  // AuthContextからuser情報を取得
+  const { user } = useAuth();
   return (
     <>
       <div>
         <h1>管理画面</h1>
+        <p>{user.name}さん、ようこそ！！</p>
         <Link to="/new-trip">
           <button>新規作成</button>
         </Link>
